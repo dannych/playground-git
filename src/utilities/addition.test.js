2c383cfa8d8cfc009049422217019f26bd023677
violin <if315030@students.del.ac.id>
2017-08-01 10:02:22 +0700

    multiplication

--- src/utilities/addition.test.js
+++ src/utilities/addition.test.js
@@ -1,5 +1,13 @@
+<<<<<<< HEAD
 import {cross} from './multiplication';
 
 it('it should cross', () => {
-  expect(cross(2,2),toBe(4);
+  expect(cross(1,1),toBe(1);
 });
+=======
+import { add } from './addition';
+
+it('it should add', () => {
+  expect(add(1, 1)).toBe(2);
+});
+>>>>>>> f/calculator
